558bee5edc8aa4b864b1605b2988239bf2bfa453
ArhamAzhar1 <arham8718290@gmail.com>
2024-11-10 23:46:20 +0500

    Further working on game

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,26 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
-using UnityEngine.Networking;
 using Unity.Netcode;
+
 
 public class PlayerController : NetworkBehaviour
 {
-
+    [SerializeField]
+    private float speed = 3f;
     private Camera _mainCamera;
-    private Vector3 _mouseInput;
+    private Vector3 _mouseInput = Vector3.zero;
+
     private void Initialize()
     {
-        _mainCamera = _mainCamera.main ;
+        _mainCamera = Camera.main;
     }
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        Initialize();
+    }
+
    private void Update()
    {
-       
-        _mouseInput.x =Input.mousePosition.x;
-        _mouseInput.y =Input.mousePosition.y;
-        _mouseInput.z =_mainCamera.nearClipPlane;
-        Vector3 mouseWorldCoordinates = _mainCamera.SceenToWorldPoint((Vector3)Input.mousePosition);
-        transform.position = new MoveTorward(current:transform.position, target:mouseWorldCoordinates, Time.deltatime);
+        if(!IsOwner || !Application.isFocused) return;
+        //Movement
+       _mouseInput.x = Input.mousePosition.x;
+       _mouseInput.y = Input.mousePosition.y;
+       _mouseInput.z = _mainCamera.nearClipPlane;
+       Vector3 mouseWorldCoordinates = _mainCamera.ScreenToWorldPoint(_mouseInput);
+       transform.position = Vector3.MoveTowards(transform.position, mouseWorldCoordinates,Time.deltaTime * speed);
+
+        //Rotation
+       if(mouseWorldCoordinates != transform.position)
+       {
+            Vector3 targetDirection = mouseWorldCoordinates - transform.position;
+            targetDirection.z = 0f;
+            transform.up = targetDirection;
+       }
    }
 }
 
