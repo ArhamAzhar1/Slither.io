f6ec217a77d6179fe824b9de23ef66b7b8149432
ArhamAzhar1 <arham8718290@gmail.com>
2024-11-19 23:43:38 +0500

    Scripting

--- Assets/Script/PlayerLength.cs
+++ Assets/Script/PlayerLength.cs
@@ -9,16 +9,27 @@ public class PlayerLength : NetworkBehaviour
     public NetworkVariable<ushort> length = new(1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
 
     private List<GameObject> _tails;
+    private Transform _lastTail;
+    private Collider2D _collider2D;
 
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
         _tails = new List<GameObject>();
+        _lastTail = transform;
+        _collider2D = GetComponent<Collider2D>();
     }
 
         private void InsantiateTail()
         {
-            GameObject tail = Instantiate(tailPrefab, transform.position, Quaternion.identity);
-            _tails.Add(tail);
+            GameObject tailGameObject = Instantiate(tailPrefab, transform.position, Quaternion.identity);
+            if (tailGameObject.TryGetComponent(out Tail tail))
+            {
+                tail.networkOwner = transform;
+                tailGameObject.followTransform = _lastTail;
+                _lastTail = tailGameObject.transform;
+                Physics2D.IgnoreCollision(tailGameObject.GetComponent<Collision2D>(), _collider2D);
+            }
+            _tails.Add(tailGameObject);
         }
 }
